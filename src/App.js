668614a49be103d309c11b6f55f66f9cf5073a14
Vivek R <r.vivek@kloctechnologies.co.in>
2020-02-12 17:27:50 +0530

    fix: Display react gauge values with the sample sensor data

--- src/App.js
+++ src/App.js
@@ -1,120 +1,120 @@
 import React, { Component }         from 'react';
-import axios                        from 'axios';
-import { compose }                  from 'recompose';
-import Geocode                      from 'react-geocode';
-import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker,
-    InfoWindow
-}                                   from 'react-google-maps';
+// import axios                        from 'axios';
+// import { compose }                  from 'recompose';
+// import Geocode                      from 'react-geocode';
+// import {
+//     withScriptjs,
+//     withGoogleMap,
+//     GoogleMap,
+//     Marker,
+//     InfoWindow
+// }                                   from 'react-google-maps';
 // import {
 //     Map,
 //     Marker,
 //     InfoWindow,
 //     GoogleApiWrapper
 // }                                   from 'google-maps-react';
-// import GaugeChart                   from 'react-gauge-chart';
+import GaugeChart                   from 'react-gauge-chart';
 // import L                            from 'leaflet';
 
-const appID = process.env.REACT_APP_APP_ID;
-const googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+// const appID = process.env.REACT_APP_APP_ID;
+// const googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 // const mapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
-    return (
-        <GoogleMap defaultZoom={8} defaultCenter={{ lat: 12.9716, lng: 77.5946 }}>
-            {props.locate.map((marker, index) => {
-                const onClick = props.onClick.bind(this, marker);
-                return (
-                    <Marker
-                        key={index}
-                        onClick={onClick}
-                        position={{ 
-                            lat: marker.results[0].geometry.location.lat,
-                            lng: marker.results[0].geometry.location.lng
-                        }}
-                    >
-                        {props.selectedMarker === marker &&
-                            <InfoWindow>
-                                <div>
-                                    {props.temp}
-                                </div>
-                            </InfoWindow>}
-                        }
-                    </Marker>
-                )
-            })}
-        </GoogleMap>
-    )
-})
+// const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
+//     return (
+//         <GoogleMap defaultZoom={8} defaultCenter={{ lat: 12.9716, lng: 77.5946 }}>
+//             {props.locate.map((marker, index) => {
+//                 const onClick = props.onClick.bind(this, marker);
+//                 return (
+//                     <Marker
+//                         key={index}
+//                         onClick={onClick}
+//                         position={{ 
+//                             lat: marker.results[0].geometry.location.lat,
+//                             lng: marker.results[0].geometry.location.lng
+//                         }}
+//                     >
+//                         {props.selectedMarker === marker &&
+//                             <InfoWindow>
+//                                 <div>
+//                                     {props.temp}
+//                                 </div>
+//                             </InfoWindow>}
+//                         }
+//                     </Marker>
+//                 )
+//             })}
+//         </GoogleMap>
+//     )
+// })
 
-export default class ShelterMap extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            shelters: [],
-            selectedMarker: false,
-            locate: [],
-            temp: null
-        }
-    }
+// export default class ShelterMap extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             shelters: [],
+//             selectedMarker: false,
+//             locate: [],
+//             temp: null
+//         }
+//     }
 
-    componentDidMount() {
-        axios.get("https://interviewapikloc.herokuapp.com/stores.json")
-        .then((response) => {
-            this.setState({ shelters: response.data });
+//     componentDidMount() {
+//         axios.get("https://interviewapikloc.herokuapp.com/stores.json")
+//         .then((response) => {
+//             this.setState({ shelters: response.data });
             
-            let myArray = [];
-            this.state.shelters.map((value) => {
-                Geocode.setApiKey(googleApiKey);
-                Geocode.enableDebug();
+//             let myArray = [];
+//             this.state.shelters.map((value) => {
+//                 Geocode.setApiKey(googleApiKey);
+//                 Geocode.enableDebug();
 
-                return Geocode
-                        .fromAddress(value.location)
-                        .then((response) => {
-                            myArray.push(response);
-                            this.setState(prevState => ({
-                                locate: [].concat(myArray)
-                            }))
-                        });
-            });
-        })
-        .catch((err) => { console.log(err); });
-    }
+//                 return Geocode
+//                         .fromAddress(value.location)
+//                         .then((response) => {
+//                             myArray.push(response);
+//                             this.setState(prevState => ({
+//                                 locate: [].concat(myArray)
+//                             }))
+//                         });
+//             });
+//         })
+//         .catch((err) => { console.log(err); });
+//     }
 
-    handleClick = (marker, event) => {
-        this.setState({ selectedMarker: marker });
+//     handleClick = (marker, event) => {
+//         this.setState({ selectedMarker: marker });
 
-        let { selectedMarker } = this.state;
-        let lat = selectedMarker.results[0].geometry.location.lat;
-        let lng = selectedMarker.results[0].geometry.location.lng;
+//         let { selectedMarker } = this.state;
+//         let lat = selectedMarker.results[0].geometry.location.lat;
+//         let lng = selectedMarker.results[0].geometry.location.lng;
 
-        axios
-            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${appID}`)
-            .then((response) => {
-                let temp = response.data.main.temp;
-                this.setState({ temp })
-            })
-    }
+//         axios
+//             .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${appID}`)
+//             .then((response) => {
+//                 let temp = response.data.main.temp;
+//                 this.setState({ temp })
+//             })
+//     }
 
-    render() {
-        return (
-            <MapWithAMarker
-                selectedMarker={this.state.selectedMarker}
-                markers={this.state.shelters}
-                onClick={this.handleClick}
-                locate={this.state.locate}
-                temp={this.state.temp}
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&v=3.exp&libraries=geometry,drawing,places`}
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
-        )
-    }
-}
+//     render() {
+//         return (
+//             <MapWithAMarker
+//                 selectedMarker={this.state.selectedMarker}
+//                 markers={this.state.shelters}
+//                 onClick={this.handleClick}
+//                 locate={this.state.locate}
+//                 temp={this.state.temp}
+//                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&v=3.exp&libraries=geometry,drawing,places`}
+//                 loadingElement={<div style={{ height: `100%` }} />}
+//                 containerElement={<div style={{ height: `400px` }} />}
+//                 mapElement={<div style={{ height: `100%` }} />}
+//             />
+//         )
+//     }
+// }
 
 // export default class App extends Component {
 //     constructor(props) {
@@ -223,88 +223,116 @@ export default class ShelterMap extends Component {
 //   apiKey: googleApiKey
 // })(MapContainer);
 
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       degrees: 0
-//     }
-//   }
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            degrees: 0
+        }
+    }
 
-//   callForWeatherData = (text) => {
-//      axios
-//       .get(`https://api.openweathermap.org/data/2.5/weather?zip=560066,in&units=metric&APPID=${appID}`)
-//       .then((response) => {
-//         console.log(response);
-//         let data = response.data;
-//         let temp = data.main.temp;
-//         let humidity = data.main.humidity;
-//         let wind = data.wind.speed;
+    callForWeatherData = (text) => {
+        let data = {
+            "deviceId": "Unit08",
+            "buildVersion": "v4.6",
+            "description": "Temp Minder",
+            "location": {
+                "lat": "58.857475",
+                "lng": "5.742801",
+                "description": "Sandnes, Norway Lab"
+            },
+            "temperature": 27.75,
+            "pressure": 962.04,
+            "humidity": 32.4
+        }
 
-//         if (text === 'Temperature') {
-//           this.setState({
-//             degrees: temp
-//           });
-//         } else if (text === 'Humidity') {
-//           this.setState({
-//             degrees: humidity
-//           });
-//         } else if (text === 'Wind') {
-//           this.setState({
-//             degrees: wind
-//           });
-//         }
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }
+        if (text === 'Temperature') {
+            this.setState({
+                degrees: data.temperature
+            });
+        } else if (text === 'Humidity') {
+            this.setState({
+                degrees: data.humidity
+            });
+        } else if (text === 'Wind') {
+            this.setState({
+                degrees: (data.pressure * 100) / 1000
+            });
+        }
 
-//   handleClick = (e) => {
-//     e.preventDefault();
-//     let text = e.target.innerText;
-//     this.callForWeatherData(text);
-//   }
+    //  axios
+    //   .get(`https://api.openweathermap.org/data/2.5/weather?zip=560066,in&units=metric&APPID=${appID}`)
+    //   .then((response) => {
+    //     console.log(response);
+    //     let data = response.data;
+    //     let temp = data.main.temp;
+    //     let humidity = data.main.humidity;
+    //     let wind = data.wind.speed;
 
-//   render() {
-//     const { degrees } = this.state;
-//     const percent = 0. + degrees;
-//     const range = percent / 100;
+    //     if (text === 'Temperature') {
+    //       this.setState({
+    //         degrees: temp
+    //       });
+    //     } else if (text === 'Humidity') {
+    //       this.setState({
+    //         degrees: humidity
+    //       });
+    //     } else if (text === 'Wind') {
+    //       this.setState({
+    //         degrees: wind
+    //       });
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  }
 
-//     return (
-//       <div style={{ backgroundColor: 'black', height: 723 }}>
-//         <div className="btn-group" role="group" style={{ display: 'flex' }}>
-//           <button
-//             type="button"
-//             className="btn btn-secondary"
-//             style={{ backgroundColor: 'lightgreen', color: 'black' }}
-//             onClick={(e) => this.handleClick(e)}
-//           >
-//             Temperature
-//           </button>
-//           <button
-//             type="button"
-//             className="btn btn-secondary"
-//             style={{ backgroundColor: 'yellow', color: 'black' }}
-//             onClick={(e) => this.handleClick(e)}
-//           >
-//             Humidity
-//           </button>
-//           <button
-//             type="button"
-//             className="btn btn-secondary"
-//             style={{ backgroundColor: 'red', color: 'black' }}
-//             onClick={(e) => this.handleClick(e)}
-//           >
-//             Wind
-//           </button>
-//         </div>
-//         <GaugeChart
-//           id="gauge-chart1"
-//           nrOfLevels={3}
-//           percent={range}
-//         />
-//       </div>
-//     )
-//   }
-// }
+  handleClick = (e) => {
+    e.preventDefault();
+    let text = e.target.innerText;
+    this.callForWeatherData(text);
+  }
+
+  render() {
+    const { degrees } = this.state;
+    const percent = 0. + degrees;
+    const range = percent / 100;
+
+    return (
+      <div style={{ backgroundColor: 'black', height: 723 }}>
+        <div className="btn-group" role="group" style={{ display: 'flex' }}>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ backgroundColor: 'lightgreen', color: 'black' }}
+            onClick={(e) => this.handleClick(e)}
+          >
+            Temperature
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ backgroundColor: 'yellow', color: 'black' }}
+            onClick={(e) => this.handleClick(e)}
+          >
+            Humidity
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ backgroundColor: 'red', color: 'black' }}
+            onClick={(e) => this.handleClick(e)}
+          >
+            Wind
+          </button>
+        </div>
+        <GaugeChart
+          id="gauge-chart1"
+          nrOfLevels={3}
+          percent={range}
+        />
+      </div>
+    )
+  }
+}
